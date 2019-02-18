8534d0863b0e463d4bcef41e60cdfe4002226e84
Kishan <kkpatel911@gmail.com>
2019-02-18 17:35:20 -0500

    created reusable components

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+
+const Players = [
+    {
+        name: "Guil",
+        score: 50,
+        id: 1
+    },
+    {
+        name: "Treasure",
+        score: 85,
+        id: 2
+    },
+    {
+        name: "Ashley",
+        score: 95,
+        id: 3
+    },
+    {
+        name: "James",
+        score: 80,
+        id: 4
+    }
+]
+    ;
+
 const Header = (props) => {
     return (
         <header>
@@ -7,37 +32,52 @@ const Header = (props) => {
     )
 }
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className='player'>
             <span className="player-name">
-                Kishan
+                {props.name}
             </span>
-            <Counter />
+            <Counter score={props.score} />
         </div>
     )
 }
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className='counter'>
             <button className='counter-action decrement'> - </button>
-            <span className='counter-score'> 20 </span>
+            <span className='counter-score'> {props.score} </span>
             <button className='counter-action increment'> + </button>
         </div>
     );
 }
 
-const App = () => {
+const App = (props) => {
     return (
         <div className="scoreboard">
-            <Header title='Scoreboard' totalPlayers={1} />
+            <Header
+                title='Scoreboard'
+                totalPlayers={props.initialPlayers.length}
+            />
             {/*  Players list*/}
-            <Player />
+            {props.initialPlayers.map(player => {
+                return (
+                    <Player
+                        name={player.name}
+                        score={player.score}
+                        key={player.id.toString()}
+                    />
+                );
+            }
+
+            )}
         </div>
     );
 }
 
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={Players} />,
     document.getElementById('root')
 )
+
+
